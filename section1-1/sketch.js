29bbb5bdfb5ed91f9a70149b60df197e839be2c3
miamyan <2190520h@gsuite.kobe-u.ac.jp>
2022-10-11 01:16:39 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -3,14 +3,14 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  background(0, 204, 102);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(255, 153, 0);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
+  fill(51, 153, 255);
   triangle(0,108,106,108,106,0);
-  fill(255);
-  textSize(32);
+  fill(255, 51, 153);
+  textSize(50);
   textFont("serif");
-  text("46", 68, 100);
+  text("78", 68, 100);
 }
